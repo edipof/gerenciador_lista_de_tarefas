cb565a46d71e23992fcc4ed7dff8096c389ef1e1
Édipo <edipofernandes7@gmail.com>
2015-06-06 15:53:32 -0300

    Implementação das classes modelo

---
+++ tp-final-pm/src/model/Usuario.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Usuario {
+	
+	private long id;
+	private String nome;
+	private ArrayList<Tarefa> tarefas = new ArrayList<Tarefa>();
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public ArrayList<Tarefa> getTarefas() {
+		return tarefas;
+	}
+	
+	public void setTarefas(ArrayList<Tarefa> tarefas) {
+		this.tarefas = tarefas;
+	}
+	
+	
+}
