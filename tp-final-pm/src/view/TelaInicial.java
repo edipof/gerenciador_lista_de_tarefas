ca77f192eb33eb55f4811fb77771d3c4e7d2d74e
Diego Aniceto <diego.henrique.aniceto@gmail.com>
2015-06-12 18:11:15 -0300

    Tela Inicial 'finalizada'

--- tp-final-pm/src/view/TelaInicial.java
+++ tp-final-pm/src/view/TelaInicial.java
@@ -1,35 +1,113 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class TelaInicial extends JFrame {
+	
+	private String listaUsuario[] = {"Fulano", "Ciclano", "Beltrano"};
+	private JTextField novoUsuario;
+	private JList listaSelecionavel;
+	private JButton entrar;
+	
+	public TelaInicial() {
+		// TODO Stub de construtor gerado automaticamente
+		super("EFIFO - Gerenciador de Tarefas");
+		
+		//Obtém painel de conteudo
+		Container container = getContentPane();
+		
+		//Configura o Layout com o BorderLayout
+		container.setLayout( new BorderLayout(30, 30));
+		
+		Box caixa[] = new Box[3];
+		
+		
+		
+		caixa[0] = Box.createHorizontalBox();
+		caixa[1] = Box.createHorizontalBox();
+		caixa[2] = Box.createHorizontalBox();
+		
+		JLabel mot = new JLabel("Entre com um Novo Usuario, ou selecione um pré-existente", SwingConstants.CENTER);  
+		
+		caixa[0].add(Box.createHorizontalStrut(20));
+		
+		caixa[0].add(mot);
+		
+		//Criando um leaiute relativo para os usuarios, tanto o novo quantos os pre-selecionados
+		
+		JPanel painelUsuario = new JPanel();
+		
+		
+		novoUsuario = new JTextField(15);
+		JLabel usuarioNovo = new JLabel("Usuário: ");
+		//Configura o layout do painel na horizotal
+		painelUsuario.add(usuarioNovo);
+		painelUsuario.add(novoUsuario);
 
-	private JFrame tela;
-	private JPanel painel;
+		entrar = new JButton("Entrar");
+		painelUsuario.add(entrar);
+		caixa[1].add(Box.createHorizontalStrut(20));
+		caixa[1].add(painelUsuario);
+		
+	
+		//Lista de Usuarios
+		
+		JPanel painelLista = new JPanel();
+		
+		listaSelecionavel = new JList(listaUsuario);
+		listaSelecionavel.setVisibleRowCount(5);
+		listaSelecionavel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		listaSelecionavel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		listaSelecionavel.setFixedCellWidth(210);
+		painelLista.add(listaSelecionavel);
+		caixa[2].add(Box.createHorizontalStrut(50));
+		caixa[2].add(painelLista);
+		
+		JPanel painelCentral = new JPanel();
+		painelCentral.add(caixa[1]);
+		painelCentral.add(caixa[2]);
+		
+		JPanel rodape = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		rodape.add(new JButton("Entrar"));
+		caixa[2].add(rodape);
+		//Anexa paineis na região do Container
+		container.add(caixa[0], BorderLayout.NORTH);
+		container.add(caixa[1], BorderLayout.CENTER);
+		container.add(caixa[2], BorderLayout.SOUTH);
+		setVisible(true);
+		setSize(500, 250);
+		
+	}
+	
+	
+	/* Comenta Tudo 
 	public void exibirTela(){
 		prepararJanela();
-		preparPainelUsuario();
+		preparPainel();
 		BotaoSair();
 		mostrarJanela();
 	}
 
 	private void inserirUsuario() {
 		
-		JPanel painelUsuario = new JPanel();
-		painelUsuario.add(new JLabel("Usuário: "));
-		painelUsuario.add(new JTextField("Insira um novo Usuário"));
+		JPanel dadoUsuario = new JPanel();
+		dadoUsuario.add(new JLabel("Usuário: "));
+		dadoUsuario.add(new JTextField("Insira um novo Usuário"));
 		
-		JPanel painelUsuarioBotao = new JPanel();
-		painelUsuarioBotao.add(new JButton("Iniciar"));
+		JPanel dadoUsuarioBotao = new JPanel();
+		dadoUsuarioBotao.add(new JButton("Iniciar"));
 		
-		painel.add(painelUsuario, BorderLayout.WEST);
-		painel.add(painelUsuarioBotao, BorderLayout.EAST);
+		painelUsuario.add(dadoUsuario);
+		painelUsuario.add(dadoUsuarioBotao);
 	}
 
 	private void prepararJanela() {
@@ -47,16 +125,37 @@ public class TelaInicial extends JFrame {
 					System.exit(0);
 				}
 			});
-		painel.add(botaoSair);
+		tela.add(botaoSair);
 	}
 
-	private void preparPainelUsuario() {
-		// TODO Stub de método gerado automaticamente
-		painel = new JPanel();
+	private void preparPainel() {
+		// prepara o painel superior (Usuario)
+		painelPrincipal = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		
-		//painel.setBounds(101, 650, 480, 173);
+		painelUsuario = new JPanel();
 		inserirUsuario();
-		tela.setContentPane(painel);
+		painelPrincipal.add(painelUsuario);
+		//==========================================================
+		
+		//Prepara Painel Inferior (Lista de Usuario)
+		
+		listaUsuarios = new JPanel();
+		criarListaUsuarios();
+		painelPrincipal.add(listaUsuarios);
+		
+		tela.setContentPane(painelPrincipal);
+	}
+
+	private void criarListaUsuarios() {
+		String teste[] = {"Fulano", "Ciclano", "Beltrano"}; 
+		JPanel painelCombo = new JPanel();
+		JComboBox dropdown = new JComboBox(teste);
+		
+		dropdown.setMaximumRowCount(3);
+		
+		painelCombo.add(dropdown);
+		listaUsuarios.add(painelCombo);
+		
 		
 	}
 
@@ -65,6 +164,8 @@ public class TelaInicial extends JFrame {
 		tela.setSize(500, 250);
 		tela.setVisible(true);	
 	}
+	
+	Fim Do Comentario */
 	
 
 }
