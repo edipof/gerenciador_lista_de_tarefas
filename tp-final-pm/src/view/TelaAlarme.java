0aa1c50fa81cd74c6877b35e65a911c8666aba43
bruno Vinicius <bruno.avila@dcc.ufmg.br>
2015-06-15 14:38:13 -0300

    Alarme estah funcionando

--- tp-final-pm/src/view/TelaAlarme.java
+++ tp-final-pm/src/view/TelaAlarme.java
@@ -1,20 +1,27 @@
 package view;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import model.Tarefa;
+
+import model.Lembrete;
+
+import java.awt.Color;
+import java.awt.Font;
 
 public class TelaAlarme extends JFrame{
 	private JPanel painel;
-	private JLabel aviso;
-	private JLabel descricao_tarefa;
+	private JLabel aviso = new JLabel();
+	private JLabel titulo = new JLabel();
+	private JLabel descricao_tarefa = new JLabel();
 	
-	public TelaAlarme(Tarefa t){
+	public TelaAlarme(Lembrete l){
 		prepararJanela();
 		preparPainelUsuario();
-		this.add(this.aviso);
-		this.add(this.descricao_tarefa);
+		this.aviso.setText("Eh hora de realizar a tarefa:");
+		this.titulo.setText(l.getTitulo());
+		this.descricao_tarefa.setText(l.getDescricao());
 		mostrarJanela();
 	}
 	public void exibirTela(){
@@ -36,14 +43,26 @@ public class TelaAlarme extends JFrame{
 		
 		//painel.setBounds(101, 650, 480, 173);
 		//inserirUsuario();
+		Font fonte_aviso = new Font(null, Font.BOLD, 24);
+		Font fonte_titulo = new Font(null, Font.PLAIN, 20);
+		Font fonte_descricao = new Font(null, Font.PLAIN, 16);
+		painel.setLayout(new BoxLayout(painel, BoxLayout.Y_AXIS));
+		this.aviso.setFont(fonte_aviso);
+		this.titulo.setFont(fonte_titulo);
+		this.descricao_tarefa.setFont(fonte_descricao);
+		painel.add(this.aviso);
+		painel.add(this.titulo);
+		painel.add(this.descricao_tarefa);
+		painel.setBackground(Color.yellow);
 		this.setContentPane(painel);
 		
 	}
 
 	private void mostrarJanela() {
 		// TODO Stub de método gerado automaticamente
-		this.setSize(300, 500);
-		this.setVisible(true);	
+		this.setSize(350, 200);
+		this.setLocation(500, 250);
+		this.setVisible(true);
 	}
 
 }
