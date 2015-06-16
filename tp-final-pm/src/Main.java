9ce201950c6eed9aa023c0d380c6cade8282f072
Lucas Barsand <barsandlucas@gmail.com>
2015-06-16 17:35:12 -0300

    diego vai tomar no cu de novo

--- tp-final-pm/src/Main.java
+++ tp-final-pm/src/Main.java
@@ -6,35 +6,14 @@ import view.TelaVisualizacaoDeTarefas;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Stub de método gerado automaticamente
-		//TelaInicial index = new TelaInicial();
-		//index.exibirTela();
 		
-		//TelaCriarTarefa criar_tarefa = new TelaCriarTarefa();
-		//criar_tarefa.exibirTela();
-		//TelaCriarTarefa app = new TelaCriarTarefa();
-//		java.awt.EventQueue.invokeLater(new Runnable() {
-//    		public void run() {
-//    			new TelaCriarTarefa().setVisible(true);
-//    		}
-//    	});
 		java.awt.EventQueue.invokeLater(new Runnable() {
+			
     		public void run() {
-//    			new TelaVisualizacaoDeTarefas("Bruno");
+    			
     			new TelaInicial();
     		}
     	});
-		/**
-	     * @param args the command line arguments
-	    *//*
-	    public static void main(String args[]) {
-	    	java.awt.EventQueue.invokeLater(new Runnable() {
-	    		public void run() {
-	    			new TelaCriarTarefa().setVisible(true);
-	    		}
-	    	});
-	    }*/
-		
 	}
 
 }
